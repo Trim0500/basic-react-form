6b433fad5307bddcd1d4ad0e9b850fbf519ad8b9
Tristan Lafleur <70395022+Trim0500@users.noreply.github.com>
2026-01-15 21:17:25 -0500

    Feat: Send todo form data

--- src/routes/forms/basic-form.tsx
+++ src/routes/forms/basic-form.tsx
@@ -1,45 +1,110 @@
 import * as React from 'react';
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider, useMutation } from '@tanstack/react-query';
 
 export const Route = createFileRoute('/forms/basic-form')({
   component: RouteComponent,
-})
+});
 
-function RouteComponent() {
-  let fullNameInputId = 'full-name-input';
+async function PostFormData(formData: string): Promise<string> {
+  const response = await fetch('http://localhost:5140/api/todo/todo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: formData,
+  });
+  const id = await response.text();
+  
+  return id;
+}
+
+function ToDoForm()
+{
+  let toDoTitleInputId = 'todo-title-input';
+  let toDoNotesInputId = 'todo-notes-input';
+  let todoCreateAtInputId = 'todo-created-at-input';
+  let todoCompletedAtInputId = 'todo-completed-at-input';
+  let toDoPriorityInputId = 'todo-priority-input';
 
   let [textInputs, setTextInputs] = React.useState({
-    [fullNameInputId]: ''
+    [toDoTitleInputId]: '',
+    [toDoNotesInputId]: '',
+    [todoCreateAtInputId]: '',
+    [todoCompletedAtInputId]: '',
+    [toDoPriorityInputId]: ''
   });
 
-  let handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  let priorityNumber = Number(textInputs[toDoPriorityInputId]);
+
+  let createdAtDate = textInputs[todoCreateAtInputId] !== '' ? new Date(textInputs[todoCreateAtInputId]) : null;
+
+  let completedAtDate = textInputs[todoCompletedAtInputId] !== '' ? new Date(textInputs[todoCompletedAtInputId]) : null;
+
+  let formData = {
+      Title: textInputs[toDoTitleInputId],
+      Notes: textInputs[toDoNotesInputId],
+      IsCompleted: false,
+      CreatedAt: createdAtDate === null ? null : createdAtDate.toISOString(),
+      CompletedAt: completedAtDate === null ? null : completedAtDate.toISOString(),
+      Priority: priorityNumber,
+      Category: "Personal"
+    };
+  let jsonFormData = JSON.stringify(formData);
+
+  let handleOnChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
 
     setTextInputs({...textInputs, [e.currentTarget.id]: e.currentTarget.value});
   };
 
+  let handleOnChangeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+
+    setTextInputs({...textInputs, [e.currentTarget.id]: e.currentTarget.value});
+  };
+  
   let navigate = useNavigate();
 
-  let HandleFormPost = async () => {
-    let response = await new Promise((resolve) => { setTimeout(() => { resolve(true); }, 1000); });
-    let success = await Boolean(response);
-    if (success)
-    {
-      navigate({ to: '/forms-submit/$basicFormId', params: { basicFormId: '123' } });
-    }
-    else
-    {
-      alert("Form submission failed. Please try again.");
-    }
-  }
-
+  const mutation = useMutation({
+    mutationFn: () => PostFormData(jsonFormData)
+                      .then((id: string) => {
+                        navigate({ to: '/forms-submit/$basicFormId', params: { basicFormId: id } });
+                      })
+  });
+  
   return (
     <>
-      <form action={HandleFormPost}>
-        <input type='text' id={fullNameInputId} value={textInputs.fullNameInputId} placeholder='Enter full name (i.e. John Doe)' onChange={handleOnChange} />
-        <input type='checkbox' id='checked-input' ></input>
-        <button type="submit">Submit Basic Form</button>
+      <form onSubmit={(e: React.FormEvent) => { e.preventDefault(); mutation.mutate(); }}>
+        <div>
+          <input type='text' id={toDoTitleInputId} value={textInputs[toDoTitleInputId]} placeholder='Enter To Do name (i.e. Some Task)' onChange={handleOnChangeInput} />
+        </div>
+        <div>
+          <textarea id={toDoNotesInputId} value={textInputs[toDoNotesInputId]} placeholder='Enter To Do notes (i.e. Some Notes)' onChange={handleOnChangeTextArea} />
+        </div>
+        <div>
+          <input type='date' id={todoCreateAtInputId} value={textInputs[todoCreateAtInputId]} placeholder='Enter To Do created at date (i.e. 2023-01-01)' onChange={handleOnChangeInput} />
+        </div>
+        <div>
+          <input type='date' id={todoCompletedAtInputId} value={textInputs[todoCompletedAtInputId]} placeholder='Enter To Do completed at date (i.e. 2023-01-01)' onChange={handleOnChangeInput} />
+        </div>
+        <div>
+          <input type='number' id={toDoPriorityInputId} value={textInputs[toDoPriorityInputId]} placeholder='Enter To Do priority (i.e. 1-5)' onChange={handleOnChangeInput} />
+        </div>
+        <div>
+          <button type="submit">Submit Basic Form</button>
+        </div>
       </form>
     </>
+  )
+}
+
+function RouteComponent() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+        <ToDoForm />
+    </QueryClientProvider>
   );
 }
